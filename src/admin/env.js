bacd62f138f0dc1c862978054b58b7880c6a0803
AbbasJafari-vu <abbas.jafari.4001@gmail.com>
2024-01-09 19:32:07 +0330

    init

--- src/admin/env.js
+++ src/admin/env.js
@@ -1,14 +1,14 @@
 export const apiVersion =
-  process.env.REACT_APP_SANITY_API_VERSION || "2023-01-01";
+    process.env.REACT_APP_SANITY_API_VERSION || "2023-01-01";
 
 export const dataset = assertValue(
-  process.env.REACT_APP_SANITY_DATASET,
-  "Missing environment variable: REACT_APP_SANITY_DATASET"
+    process.env.REACT_APP_SANITY_DATASET,
+    "Missing environment variable: REACT_APP_SANITY_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.REACT_APP_SANITY_PROJECT_ID,
-  "Missing environment variable: REACT_APP_SANITY_PROJECT_ID"
+    process.env.REACT_APP_SANITY_PROJECT_ID,
+    "Missing environment variable: REACT_APP_SANITY_PROJECT_ID"
 );
 
 export const readToken = process.env.SANITY_API_READ_TOKEN;
@@ -18,9 +18,9 @@ export const previewSecretDocumentId = "preview.secret";
 export const useCdn = false;
 
 function assertValue(v, errorMessage) {
-  if (v === undefined) {
-    throw new Error(errorMessage);
-  }
+    if (v === undefined) {
+        throw new Error(errorMessage);
+    }
 
-  return v;
+    return v;
 }
